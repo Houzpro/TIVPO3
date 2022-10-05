eaf14a18b77954843263bb66f49aceafee89be6e
Splesh <38045850+Ace0fSpades@users.noreply.github.com>
2022-10-06 00:09:14 +0300

    Update Converter.java
    Тело main

--- Converter.java
+++ Converter.java
@@ -12,5 +12,36 @@ public class Converter {
     public double rub_uan (double r) {
         return r / 8.36;
     }
+    
+     public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+        System.out.println("[1]Перевести RUB в DOL\n[2]Перевести RUB в EUR\n[3]Перевести RUB в UAN\n[0]Выход\nВыберете действие:");
+        int choose = in.nextInt();
+
+
+        switch (choose) {
+            case 1: {
+                System.out.println("Введите кол-во RUB: ");
+                double num = in.nextDouble();
+                System.out.println("DOL: "+rub_dol(num));
+                break;
+            }
+            case 2: {
+                System.out.println("Введите кол-во RUB: ");
+                double num = in.nextDouble();
+                System.out.println("EUR: "+rub_eur(num));
+                break;
+            }
+            case 3: {
+                System.out.println("Введите кол-во RUB: ");
+                double num = in.nextDouble();
+                System.out.println("UAN: "+rub_uan(num));
+                break;
+            }
+
+        }
+
+    }
 
 }
