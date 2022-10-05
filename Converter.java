782b0949718f7db8a080e023d4b722ff536f0ed0
Maxim Andriewskii <56740309+millitantcat@users.noreply.github.com>
2022-10-06 00:08:47 +0300

    Конвертер Рубль-Юань

--- Converter.java
+++ Converter.java
@@ -5,9 +5,12 @@ public class Converter {
     public double rub_dol (double r) {
         return r / 59.4;
     }
-    
-    public static double rub_eur (double r) {
+
+    public double rub_eur (double r) {
         return r / 58.06;
     }
-    
+    public double rub_uan (double r) {
+        return r / 8.36;
+    }
+
 }
